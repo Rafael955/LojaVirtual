2f7db3bc759e1e5d85f1416a4e59d47eaafda28e
Rafael Caffonso <rafaelcaffonso@gmail.com>
2021-06-12 13:23:11 -0300

    Construindo controller de Colaboradores

--- LojaVirtual.Domain/Models/Colaborador.cs
+++ LojaVirtual.Domain/Models/Colaborador.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using LojaVirtual.Domain.Libraries;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace LojaVirtual.Domain.Models
@@ -12,12 +14,25 @@ namespace LojaVirtual.Domain.Models
             Id = Guid.NewGuid();
         }
 
+        [Required(ErrorMessageResourceType = typeof(MsgErro), ErrorMessageResourceName = "MsgErro001")]
+        [MinLength(4, ErrorMessageResourceType = typeof(MsgErro), ErrorMessageResourceName = "MsgErro002")]
         public string Nome { get; set; }
+
+        [Display(Name = "E-mail")]
+        [Required(ErrorMessageResourceType = typeof(MsgErro), ErrorMessageResourceName = "MsgErro001")]
+        [EmailAddress(ErrorMessageResourceType = typeof(MsgErro), ErrorMessageResourceName = "MsgErro004")]
         public string Email { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(MsgErro), ErrorMessageResourceName = "MsgErro001")]
+        [MinLength(6, ErrorMessageResourceType = typeof(MsgErro), ErrorMessageResourceName = "MsgErro002")]
         public string Senha { get; set; }
+
+        [NotMapped]
+        public string ConfirmaSenha { get; set; }
 
         private string _tipo;
 
+        [Required(ErrorMessageResourceType = typeof(MsgErro), ErrorMessageResourceName = "MsgErro001")]
         public string Tipo
         {
             get { return _tipo; }
---
+++ LojaVirtual/Areas/Colaborador/Controllers/ColaboradorController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MeuColaborador = LojaVirtual.Domain.Models.Colaborador;
+
+namespace LojaVirtual.Areas.Colaborador.Controllers
+{
+    [Area("Colaborador")]
+    [Route("[area]/[controller]")]
+    public class ColaboradorController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Cadastrar([FromForm] MeuColaborador colaborador)
+        {
+            return View();
+        }
+
+        [HttpGet("[action]/{id:guid}")]
+        public async Task<IActionResult> Atualizar(Guid id)
+        {
+            return View();
+        }
+
+        [HttpPost("[action]/{id:guid}")]
+        public async Task<IActionResult> Atualizar([FromForm] MeuColaborador colaborador, Guid id)
+        {
+            return View();
+        }
+
+        [HttpGet("[action]/{id:guid}")]
+        public async Task<IActionResult> Excluir(Guid id)
+        {
+            return View();
+        }
+    }
+}
